ea0b75980e3f7e29c36b59b03acc33aa9d9b24aa
Christopher CHEN <chenshaoming@appgame.com>
2016-07-21 12:43:28 +0800

    feat: update alipay config

--- phwoolcon-package/config/payment.php
+++ phwoolcon-package/config/payment.php
@@ -4,11 +4,33 @@ return [
     'gateways' => [
         'alipay' => [
             'description' => 'Alipay',
+            'order_prefix' => 'ALIPAY',
             'methods' => [
                 'mobile_web' => [
-                    'class' => 'Phwoolcon\Payment\Alipay\MobileWebPay',
+                    'class' => 'Phwoolcon\Payment\Method\Alipay\MobileWebPay',
                     'description' => 'Alipay Mobile Web Pay',
                 ],
+            ],
+            'partner' => 'PARTNER_ID',
+            'seller_id' => 'seller@phwoolcon.com',
+            'charset' => 'utf-8',
+            'sign_type' => 'RSA',
+            'private_key' => '-----BEGIN RSA PRIVATE KEY-----
+YOUR_PRIVATE_KEY_HERE
+-----END RSA PRIVATE KEY-----',
+            'ali_public_key' => '-----BEGIN PUBLIC KEY-----
+ALI_PUBLIC_KEY_HERE
+-----END PUBLIC KEY-----',
+            'return_url' => 'alipay/success',
+            'notify_url' => 'api/alipay/callback',
+            'required_callback_parameters' => [
+                'trade_no',
+                'out_trade_no',
+                'trade_status',
+                'total_fee',
+                'notify_id',
+                'sign_type',
+                'sign',
             ],
         ],
     ],
